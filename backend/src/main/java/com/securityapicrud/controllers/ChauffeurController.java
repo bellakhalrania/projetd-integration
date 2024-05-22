268d66c6ebc135c6ac3c73e52e4703690940a9aa
bellakhalrania <bellakhalrania12@gmail.com>
2024-05-22 21:12:25 +0100

    new commit

--- backend/src/main/java/com/securityapicrud/controllers/ChauffeurController.java
+++ backend/src/main/java/com/securityapicrud/controllers/ChauffeurController.java
@@ -1,13 +1,20 @@
 package com.securityapicrud.controllers;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+
 import com.securityapicrud.entity.Chauffeur;
+import com.securityapicrud.entity.Role;
+import com.securityapicrud.entity.User;
+import com.securityapicrud.repository.RoleRepository;
+import com.securityapicrud.repository.UserRepository;
 import com.securityapicrud.services.ChauffeurService;
 
 
@@ -15,15 +22,50 @@ import com.securityapicrud.services.ChauffeurService;
 @RequestMapping("/api/chauffeurs")
 
 public class ChauffeurController {
+	 @Autowired
+	    private UserRepository userRepository;
+
+	    @Autowired
+	    private RoleRepository roleRepository;
+
+	    @Autowired
+	    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private ChauffeurService chauffeurService;
 
     @PostMapping
-    public ResponseEntity<Chauffeur> createChauffeur(@RequestBody Chauffeur chauffeur) {
+    public ResponseEntity<?> createChauffeur(@RequestBody Chauffeur chauffeur) {
         try {
+        	if(userRepository.existsByUsername(chauffeur.getNom())){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Username is already taken!");
+            }
+
+            // add check for email exists in DB
+            if(userRepository.existsByEmail(chauffeur.getEmail())){
+            	return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("email is already taken!");
+
+            }
+
+            // create user object
+            User user = new User();
+            user.setName(chauffeur.getNom());
+            user.setUsername(chauffeur.getPrenom());
+            user.setEmail(chauffeur.getEmail());
+            user.setPassword(passwordEncoder.encode(chauffeur.getPassword()));
+            
+             
+            Role roles = roleRepository.findByName("ROLE_CHAUFFEUR").orElseThrow(() -> new RuntimeException("Role not found: ROLE_Chauffeur"));
+            user.setRoles(Collections.singleton(roles));
+
+            userRepository.save(user);
+            
             Chauffeur createdChauffeur = chauffeurService.createChauffeur(chauffeur);
             return ResponseEntity.status(HttpStatus.CREATED).body(createdChauffeur);
+            
+            
+            
+            
         } catch (Exception e) {
             // Gérer l'exception ici, par exemple, renvoyer une réponse avec un statut d'erreur 500
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
