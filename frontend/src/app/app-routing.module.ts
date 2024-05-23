7ede31fcfa8c520983bf071bcfca7a04b3d7422d
SoltaniNebras <soltaninebras304@gmail.com>
2024-05-23 12:42:53 +0100

    dashh

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -32,7 +32,7 @@ const routes: Routes = [
   {path:'signup',component:SignupComponent},
   {path:'chauffeur',component:ChauffeurLayoutComponent,children:[
     {path:'',loadChildren:()=>import('./views/chauffeur/dashboard/dashboard/dashboard.module').then(m=>m.DashboardModule)},
-    {path:'/ajouterDemande',loadChildren:()=>import('./views/chauffeur/ajouterDemande/ajouter-demande/ajouter-demande.module').then(m=>m.ajouterDemandeModule)},
+    //{path:'/ajouterDemande',loadChildren:()=>import('./views/chauffeur/ajouterDemande/ajouter-demande/ajouter-demande.module').then(m=>m.AjouterDemandeModule)},
 
   ]},
   
--- frontend/src/app/views/chauffeur/dashboard/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/views/chauffeur/dashboard/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from 'src/app/views/admin/dashboard/dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path:'',component:DashboardComponent}
-
 ];
 
 @NgModule({
